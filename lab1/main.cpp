ae92bdf702c10e0cd0653493c930cdfea8ee3cbe
1ceHat <artemdudorov2015@gmail.com>
2023-12-24 07:59:39 +0500

    Complete first lab
    Написан код по первой лабораторной работе. Выполнено 3 задание с циклами:
    вывести все число от 0 до n;
    вывести все числа от 0 до n, но вместо нечетных вывести b;
    вывести количество тысяч итераций.

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,6 +4,42 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    setlocale(LC_ALL, "Russian");
+    int num, i=0;
+
+    // №1
+    cout << "Input number: " << endl;
+    cin >> num;
+    for (int i=0; i<=num; i++)
+    {
+        cout<<i<<' ';
+    }
+
+
+    // №2
+    wcout << L"\nВведи число: " << endl;
+    cin >> num;
+
+    while (i<num)
+    {
+        if (i%2 == 0) {
+            cout<<i<<' ';
+        }
+        else{
+            cout<<"b"<<' ';
+        }
+        i++;
+    }
+
+
+    // №3
+    wcout << L"\nВведи четырёхзначное число: " << endl;
+    cin >> num;
+    for (int i=1; i<=num; i++)
+    {
+        if(i%1000==0)
+    {
+        wcout<<L"Прошла " <<i/1000<< L" тысяча итераций"<< endl;
+    }
+    }
 }
